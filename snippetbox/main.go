6eee90ea47fdf98d1654345db00a7a80d70532cd
Estifanos <95101485+justEstif@users.noreply.github.com>
2024-10-19 10:11:50 -0400

    Add basic server

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
+
+// define a home handler function which writes a byte slice containing
+// "Hello from Snippetbox" as the response body
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello from Snippetbox"))
+}
 
 func main() {
-	fmt.Println("Hello world!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	// print a log message to say that the server is starting
+	log.Print("starting server on :4000")
+
+	// use the http.listenAndServe() function to start a web server. We pass in
+	// two parameters: the TCP network address to listen on (in this case ":4000")
+	// and the servemux we just created. If http.listenAndServe() returns an error
+	// that any error return by http.listenAndServe() is always non-nil
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
 }
