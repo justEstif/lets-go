062ea9ef892f54c483647284d9f9bf0490b5023a
Estifanos <95101485+justEstif@users.noreply.github.com>
2024-10-19 10:16:52 -0400

    Add route handlers for /snippet/view and /snippet/create

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -5,23 +5,28 @@ import (
 	"net/http"
 )
 
-// define a home handler function which writes a byte slice containing
-// "Hello from Snippetbox" as the response body
 func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
+}
+
+// add a snippetView handler function
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippetbox..."))
+}
+
+// add a snippetCreate handler function
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a form for creating a new snippet..."))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	// print a log message to say that the server is starting
 	log.Print("starting server on :4000")
 
-	// use the http.listenAndServe() function to start a web server. We pass in
-	// two parameters: the TCP network address to listen on (in this case ":4000")
-	// and the servemux we just created. If http.listenAndServe() returns an error
-	// that any error return by http.listenAndServe() is always non-nil
 	err := http.ListenAndServe(":4000", mux)
 	log.Fatal(err)
 }
